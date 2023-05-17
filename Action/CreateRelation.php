6837e41ea01ac3fe273918bfe0582a4a269fb051
Vheins <rheza.ids@gmail.com>
2023-05-17 14:47:42 +0700

    Add hasManyThrough & hasOne

--- Action/CreateRelation.php
+++ Action/CreateRelation.php
@@ -29,14 +29,20 @@ class CreateRelation
             }
 
             switch ($k) {
+                case "BelongsTo":
+                    $this->belongsTo($k, $v);
+                    break;
                 case "HasOne":
-                    $this->hasone($k, $v);
+                    $this->hasOne($k, $v);
+                    break;
+                case "HasOneThrough":
+                    $this->hasManyThrough($k, $v);
                     break;
                 case "HasMany":
                     $this->hasMany($k, $v);
                     break;
-                case "BelongsTo":
-                    $this->belongsTo($k, $v);
+                case "HasManyThrough":
+                    $this->hasManyThrough($k, $v);
                     break;
             }
         }
@@ -61,6 +67,33 @@ class CreateRelation
         }
     }
 
+    private function hasOneThrough($k, $v)
+    {
+        $v = Arr::sortDesc($v);
+        foreach ($v as $key => $val) {
+            $model = file_get_contents($this->modelFile);
+
+            //Add class refferences || Check if model references exist
+            $class = "use IDS\\" . $this->module . "\\Models\\" . $val . ";";
+            $contains = Str::contains($model, $class);
+            if (!$contains)
+                $model = str_replace('//Class Refferences', "//Class Refferences\n" . $class, $model);
+
+            //Add class refferences || Check if model references exist
+            $class = "use IDS\\" . $this->module . "\\Models\\" . $key . ";";
+            $contains = Str::contains($model, $class);
+            if (!$contains)
+                $model = str_replace('//Class Refferences', "//Class Refferences\n" . $class, $model);
+
+            $strKey = Str::of($key);
+            $strVal = Str::of($val);
+            $model = str_replace('//Model Relationship', "//Model Relationship\n\tpublic function " . $strKey->camel()->plural() . "(): " . $k . "\n\t{\n\t\treturn " . '$this->' . Str::camel($k) . "(\n\t\t\t" . $strKey->studly() . "::class,\n\t\t\t" . $strVal->studly() . "::class,\n\t\t\t'" . Str::snake($this->name) . "_id',\n\t\t\t'id',\n\t\t\t'id',\n\t\t\t'" . $strKey->snake() . "_id'\n\t\t)->latestOfMany();\n\t}\n", $model);
+
+
+            file_put_contents($this->modelFile, $model);
+        }
+    }
+
     private function hasMany($k, $v)
     {
         $v = Arr::sortDesc($v);
@@ -76,6 +109,33 @@ class CreateRelation
 
             $mm = Str::of($m);
             $model = str_replace('//Model Relationship', "//Model Relationship\n\tpublic function " . $mm->camel()->plural() . "(): " . $k . "\n\t{\n\t\treturn " . '$this->' . Str::camel($k) . "(" . $mm->studly() . "::class);\n\t}\n", $model);
+            file_put_contents($this->modelFile, $model);
+        }
+    }
+
+    private function hasManyThrough($k, $v)
+    {
+        $v = Arr::sortDesc($v);
+        foreach ($v as $key => $val) {
+            $model = file_get_contents($this->modelFile);
+
+            //Add class refferences || Check if model references exist
+            $class = "use IDS\\" . $this->module . "\\Models\\" . $val . ";";
+            $contains = Str::contains($model, $class);
+            if (!$contains)
+                $model = str_replace('//Class Refferences', "//Class Refferences\n" . $class, $model);
+
+            //Add class refferences || Check if model references exist
+            $class = "use IDS\\" . $this->module . "\\Models\\" . $key . ";";
+            $contains = Str::contains($model, $class);
+            if (!$contains)
+                $model = str_replace('//Class Refferences', "//Class Refferences\n" . $class, $model);
+
+            $strKey = Str::of($key);
+            $strVal = Str::of($val);
+            $model = str_replace('//Model Relationship', "//Model Relationship\n\tpublic function " . $strKey->camel()->plural() . "(): " . $k . "\n\t{\n\t\treturn " . '$this->' . Str::camel($k) . "(\n\t\t\t" . $strKey->studly() . "::class,\n\t\t\t" . $strVal->studly() . "::class,\n\t\t\t'" . Str::snake($this->name) . "_id',\n\t\t\t'id',\n\t\t\t'id',\n\t\t\t'" . $strKey->snake() . "_id'\n\t\t)->latestOfMany();\n\t}\n", $model);
+
+
             file_put_contents($this->modelFile, $model);
         }
     }
